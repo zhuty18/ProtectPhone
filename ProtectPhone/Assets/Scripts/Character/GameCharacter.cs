b12b9ffa7c96112e56b9f537038c68525707c458
陈英发 <chenyingfa1999@qq.com>
2020-10-31 16:26:19 +0800

    declare basic interface of main classes

---
+++ ProtectPhone/Assets/Scripts/Character/GameCharacter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCharacter : MonoBehaviour
+{
+    public string name;
+    public int hp;
+    public int maxHp;
+    public float moveSpeed;
+
+    private List<Effect> effects;
+    private List<Tool> backpack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void GetDir() {}
+    public void GetPos() {}
+
+    public void AddEffect(Effect effect) {}
+
+    public void IncreaseHp(int amount) {}
+
+    public void IncreaseMaxHp(int amount) {}
+
+    public void IncreaseMoveSpeed(float amount) {}
+
+    public void IncreaseEnergy(int amount) {}
+
+    public void GainReward(Pack pack) {}
+
+    public void BeDamaged(DamageCarrier damageCarrier) {}
+
+    public void AfterAttack(GameCharacter damaged) {}
+
+    private void UseTool(Tool tool) {}
+
+    private void IsDead() {}
+
+    private void IsOnGround() {}    
+}
---
+++ ProtectPhone/Assets/Scripts/Character/NPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : GameCharacter
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeInteracted(GameCharacter interacter) {}
+
+    public bool ReceiveTool(Tool tool, int count) {}
+}
