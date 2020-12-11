98d1e6fdb8ea1c6bc007479607c6e690c6b270b7
cobs14 <44751106+cobs14@users.noreply.github.com>
2020-12-11 20:47:12 +0800

    basic movement

--- ProtectPhone/Assets/Scripts/Character/Player.cs
+++ ProtectPhone/Assets/Scripts/Character/Player.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class Player : GameCharacter
 {
     public int jumpTime;
+    public int strength;
+    public int maxStrength;
+    public int maxSpeed;
+    public int commonSpeed;
+    public float healthRecoveryTimer;
     public GameObject bullet;
     // Start is called before the first frame update
     void Start()
     {
+        hp = 50;
+        maxHp = 100;
         moveSpeed = 10;
-        jumpForce = 5;
-        jumpTime = 0;
+        maxSpeed = 20;
+        commonSpeed = 10;
+        maxStrength = 200;
+        strength = 0;
+        healthRecoveryTimer = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.rotation = Quaternion.Euler(0.0f,0.0f,0.0f);
         float hztMove = Input.GetAxis("Horizontal");
         float dir=Input.GetAxisRaw("Horizontal");
         if(dir<0){
@@ -25,7 +36,11 @@ public class Player : GameCharacter
         else if(dir>0){
             direction=1;
         }
+        
+        Vector3 oScale = transform.localScale;
+        transform.localScale = new Vector3(direction * System.Math.Abs(oScale.x), oScale.y, oScale.z);
 
+        Debug.Log(IsGrounded());
         if(body.velocity.y<=0&&IsGrounded())
         {
             jumpTime=0;
@@ -46,9 +61,36 @@ public class Player : GameCharacter
             //body.AddForce(new Vector2(0, jumpForce));
             body.velocity = new Vector2(body.velocity.x,jumpForce);
         }
+        
         if(Input.GetKeyDown(KeyCode.J))
         {
             Fire();
+        }
+
+        // Strength
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            if(strength > 0){
+                strength--;
+                moveSpeed = maxSpeed;
+            } else{
+                moveSpeed = commonSpeed;
+            }
+        } else {
+            moveSpeed = commonSpeed;
+            if(strength < maxStrength){
+                strength++;
+            }
+        }
+
+        // HP
+        healthRecoveryTimer -= Time.deltaTime;
+        if (healthRecoveryTimer <= 0) {
+            if(hp < maxHp)
+            {
+                hp += 1;
+            }
+            healthRecoveryTimer = 2.0f;
         }
     }
 
@@ -56,12 +98,14 @@ public class Player : GameCharacter
     {
         Vector2 position = transform.position;
         Vector2 direction = Vector2.down;
-        float distance = 1.0f;
+        float distance = 1.5f;
         
         RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        Debug.Log(hit.collider);
         if (hit.collider != null) {
             return true;
         }
+
         return false;
     }
 
