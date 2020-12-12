d4465dfe0ee2e29efc230af5b4b917f28d9b8332
zhuty18 <zhuty18@mails.tsinghua.edu.cn>
2020-12-12 16:25:16 +0800

    增加Camera Controller

---
+++ ProtectPhone/Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playertrack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playertrack.position.x , playertrack.position.y, -10f);
+    }
+}
